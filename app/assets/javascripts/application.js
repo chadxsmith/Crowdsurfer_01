15dd048460045de381f12e2ee3dd3ec995afaeb9
ecgeiser <erica.geiser@gmail.com>
2014-11-08 11:02:17 -0500

    added routes, controller actions, and js for yelp api call

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,40 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+
+$(document).ready(function(){
+
+  $("#search").on('submit', function(event){
+    event.preventDefault();
+    searchTerm = $('#genre').val();
+
+  $.post('/search', { genre: searchTerm }, function(data){
+    console.log(data);
+    
+    $('#results').html('');
+    for (var i = 0; i < data.businesses.length; i++) {
+      // console.log(data.businesses[i]);
+      var venue = data.businesses[i];          
+      var address = venue['location']['display_address'];
+
+      if (address[2]) {
+        var addressBlock = address[0] + '<br>' + address[1] + '<br>' + address[2];
+      } else {
+        var addressBlock = address[0] + '<br>' + address[1] + '<br> ';
+      };
+
+      if (venue['image_url']) {
+        venueImg = venue['image_url'];
+      } else {
+        venueImg = 'http://media.miamimusicweek.com/2012/12/venue-default.jpg';
+      };
+
+      $('#results').append("<div class='venue'><h3><a href='" + venue['url'] + "' target='_blank'>" + venue['name'] + "</a></h3><img src='" + venueImg + "'><p>" + addressBlock + "</p></div>");
+    }
+  });
+
+    $('#genre').val('');
+  });
+
+});
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def index
+  end
+
+  def search
+    genre = params[:genre]
+    loc = "20005"
+    parameters = { limit: 10, term: genre}
+    render json: Yelp.client.search(loc, parameters)
+  end
 end
